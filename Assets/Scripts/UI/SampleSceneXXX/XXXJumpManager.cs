ac5c2b67392a33f37e7e1b99ba90f3cafd96e77b
nanmanzixi <947407982@qq.com>
2026-08-13 22:23:13 +0800

    目录文件夹模版补充

---
+++ Assets/Scripts/UI/SampleSceneXXX/XXXJumpManager.cs
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) PeroPeroGames Co., Ltd.
+ * Author: nmzx
+ * Created On: 2025/12/24
+ * Description: XXXJumpManager类
+ */
+
+namespace UI.SampleSceneXXX
+{
+    internal sealed class XXXJumpManager
+    {
+    }
+}
